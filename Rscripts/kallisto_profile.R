f3064384c7ddc42e68f22e25b5ac1f4442b27df0
Jason Stajich <jasonstajich.phd@gmail.com>
2020-02-07 10:48:31 -0800

    only looking at condition not genotype

---
+++ Rscripts/kallisto_profile.R
@@ -0,0 +1,90 @@
+library(DESeq2)
+library(tximport)
+library(dplyr)
+library(ggplot2)
+library(magrittr)
+library(Biobase)
+library(pheatmap)
+library(RColorBrewer)
+
+samples <- read.csv("samples.csv",header=TRUE)
+exprnames <- do.call(paste,c(samples[c("Condition","Replicate")],sep="."))
+exprnames <- sub(".([123])$",".r\\1",exprnames,perl=TRUE)
+files <- file.path("results",exprnames,"abundance.h5")
+txi.kallisto <- tximport(files, type = "kallisto", txOut = TRUE)
+head(txi.kallisto$counts)
+colnames(txi.kallisto$counts) <- exprnames
+colnames(txi.kallisto$abundance) <- exprnames
+write.csv(txi.kallisto$abundance,"reports/kallisto_single.TPM.csv")
+write.csv(txi.kallisto$counts,"reports/kallisto_single.counts.csv")
+
+# DEseq2 analyses
+treatment = factor (samples$Condition)
+
+sampleTable <- data.frame(condition = treatment)
+rownames(sampleTable) = exprnames
+
+dds <- DESeqDataSetFromTximport(txi.kallisto,sampleTable,~ condition)
+
+#nrow(dds)
+dds <- dds[ rowSums(counts(dds)) > 1, ]
+#nrow(dds)
+
+dds <- estimateSizeFactors(dds)
+vsd <- vst(dds, blind=FALSE)
+rld <- rlog(dds, blind=FALSE)
+head(assay(vsd), 3)
+
+df <- bind_rows(
+  as_data_frame(log2(counts(dds, normalized=TRUE)[, 1:2]+1)) %>%
+         mutate(transformation = "log2(x + 1)"),
+  as_data_frame(assay(rld)[, 1:2]) %>% mutate(transformation = "rlog"),
+  as_data_frame(assay(vsd)[, 1:2]) %>% mutate(transformation = "vst"))
+
+colnames(df)[1:2] <- c("x", "y")
+
+pdf("plots/RNASeq_kallisto_single.pdf")
+ggplot(df, aes(x = x, y = y)) + geom_hex(bins = 80) +
+  coord_fixed() + facet_grid( . ~ transformation)
+
+select <- order(rowMeans(counts(dds,normalized=TRUE)),
+                decreasing=TRUE)[1:75]
+df <- as.data.frame(colData(dds)[,c("condition")])
+rownames(df) = exprnames
+colnames(df) = c("Condition")
+
+pheatmap(assay(vsd)[select,], cluster_rows=FALSE, show_rownames=TRUE,
+         fontsize_row = 7,fontsize_col = 7,
+         cluster_cols=FALSE, annotation_col=df,main="VSD ordered")
+
+topVar <- head(order(rowVars(assay(vsd)),
+                                decreasing=TRUE),60)
+mat  <- assay(vsd)[ topVar, ]
+mat  <- mat - rowMeans(mat)
+pheatmap(mat, show_rownames=TRUE,
+         fontsize_row = 7,fontsize_col = 7,
+         cluster_cols=FALSE, annotation_col=df,main="VSD - most different")
+
+pheatmap(assay(rld)[select,], cluster_rows=FALSE, show_rownames=TRUE,
+         fontsize_row = 7,fontsize_col = 7,
+         cluster_cols=FALSE, annotation_col=df,main="RLD")
+
+sampleDists <- dist(t(assay(vsd)))
+sampleDistMatrix <- as.matrix(sampleDists)
+rownames(sampleDistMatrix) <- paste(vsd$condition, sep="-")
+colnames(sampleDistMatrix) <- NULL
+colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+
+pheatmap(sampleDistMatrix,
+         clustering_distance_rows=sampleDists,
+         clustering_distance_cols=sampleDists,
+         col=colors)
+
+pcaData <- plotPCA(vsd, intgroup=c("condition"), returnData=TRUE)
+percentVar <- round(100 * attr(pcaData, "percentVar"))
+
+ggplot(pcaData, aes(PC1, PC2, color=condition)) +
+    geom_point(size=3) +
+    xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+    ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+    coord_fixed()
