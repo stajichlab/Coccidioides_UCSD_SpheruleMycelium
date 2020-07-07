e747714ec33e8a6aa7a6c5e767ca03c94057445e
Jason Stajich <jasonstajich.phd@gmail.com>
2020-07-07 15:54:13 -0700

    migrating to RS1 and RS2 separate results

--- pipeline/RS1_DESeq.sh
+++ pipeline/RS1_DESeq.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes 1 --ntasks 1 --out logs/DEseq_RS1.log --mem 4G 
 #SBATCH --time 2:00:00 -p short
-
+module load R
+module load R/3.6.0
 mkdir -p plots reports
 Rscript Rscripts/kallisto_profile_RS1.R
 
--- pipeline/RS2_DESeq.sh
+++ pipeline/RS2_DESeq.sh
@@ -2,6 +2,8 @@
 #SBATCH --nodes 1 --ntasks 1 --out logs/DEseq.log --mem 4G 
 #SBATCH --time 2:00:00 -p short
 
+module unload R
+module load R/3.6.0
 mkdir -p plots reports
 Rscript Rscripts/kallisto_profile_RS2.R
 
